bcf0512e8c66b5aefa1ae3609d475a5a781a8fae
Suraj Kumar <surajumang08@gmail.com>
2017-02-12 06:22:09 +0530

    file will demostrate the use of fork()

--- src/aa.c
+++ src/aa.c
@@ -1,11 +1,31 @@
 #include<stdio.h>
+#include<unistd.h>
+/* It will demonstrate the fork() system call
+
+*/
 
 int main()
 {
-//this file is present insife the src directory
 
-int k=10;
-for(int i=0;i<k;i++)
-printf("i is now : %d ",i);
+int val=10;
+pid_t pid=fork();
+
+if(pid<0){
+  fprintf(stderr,"fork failed\n");
+  return -1;
+}
+else if(pid==0){//child process
+  val+=5;
+  return 0;
+}
+else{ //the parent process
+  printf("val here is : %d",val);
+  printf("child pid is : %d \n",pid);
+  printf("parent pid is : %d\n ",getpid());
+  wait(NULL); //wait for child to complete
+  return 0;
+}
+
+
 
 }
